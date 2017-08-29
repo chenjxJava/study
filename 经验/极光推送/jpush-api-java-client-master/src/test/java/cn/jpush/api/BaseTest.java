0bd5757f55d4f63c5b7dbd05b41401df9ec42287
chenjxJava <chenjx_java@163.com>
2017-08-29 18:05:53 +0800

    0829
    0829

---
+++ 经验/极光推送/jpush-api-java-client-master/src/test/java/cn/jpush/api/BaseTest.java
@@ -0,0 +1,28 @@
+package cn.jpush.api;
+
+import org.junit.Before;
+
+public abstract class BaseTest {
+
+	protected static final String APP_KEY = "2266959deade9d98404fd433";
+    protected static final String MASTER_SECRET = "ac401b6dd037bad0e9ee910c";
+    protected static final String GROUP_MASTER_SECRET = "b11314807507e2bcfdeebe2e";
+    protected static final String GROUP_PUSH_KEY = "2c88a01e073a0fe4fc7b167c";
+
+    public static final String ALERT = "JPush Test - alert";
+    public static final String MSG_CONTENT = "JPush Test - msgContent";
+    
+    public static final String REGISTRATION_ID1 = "0900e8d85ef";
+    public static final String REGISTRATION_ID2 = "0a04ad7d8b4";
+    public static final String REGISTRATION_ID3 = "18071adc030dcba91c0";
+
+    
+    protected JPushClient jpushClient = null;
+    
+    @Before
+    public void before() {
+        jpushClient = new JPushClient(MASTER_SECRET, APP_KEY);
+        
+    }
+
+}
